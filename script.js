4538bf2b1a13017fa13ecac0f6eec0d67d280a4e
GabrielDSanta <eloviveiros@hotmail.com>
2022-09-09 20:45:04 -0300

    Function 02

--- script.js
+++ script.js
@@ -12,16 +12,20 @@ let choice2
 
 
 while(choice){
-    choice2 = prompt("1 /// Cadastrar Livro /// 2 Procurar Livro Pelo Autor /// 3 Finalizar")
+    choice2 = prompt("1 /// Cadastrar Livro /// 2 Procurar Livro Pelo Autor /// 3 Procurar Livros Pelo Ano /// 4 Finalizar")
     if(choice2 == "1"){
         cadastro()
     }
 
     if(choice2 == "2"){
-        ShowBooks()
+        ShowBooksByAutor()
     }
 
     if(choice2 == "3"){
+        ShowBooksByYear()
+    }
+
+    if(choice2 == "4"){
         choice = false
     }
 }
@@ -35,10 +39,10 @@ function cadastro(){
 }
 
 
-function ShowBooks(){
+function ShowBooksByAutor(){
     let contador2 = 0
     let LivrosAutorX = []
-    let autor = prompt("Qual o nome do Autor que você deseja ver os livros ?")
+    let autor = prompt("Qual o nome do Autor que você deseja ver os Livros ?")
     for(let index = 0; index < bookWriter.length; index++){
         if(autor == bookWriter[index]){
             LivrosAutorX[contador2] = bookName[index]
@@ -47,4 +51,18 @@ function ShowBooks(){
     }
 
     return alert(`Os Livros do Autor ${autor}: ${LivrosAutorX}`)
+}
+
+function ShowBooksByYear(){
+    let contador3 = 0
+    let LivrosAnoX = []
+    let ano = prompt("Qual o Ano que você deseja ver os Livros ?")
+    for(let index3 = 0; index3 < bookYear.length; index3++){
+        if(ano == bookYear[index3]){
+            LivrosAnoX[contador3] = bookName[index3]
+            contador3++
+        }
+    }
+
+    return alert(`Os Livros do Ano ${ano}: ${LivrosAnoX}`)
 }
